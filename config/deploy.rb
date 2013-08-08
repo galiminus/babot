96b8b1594b0c45f285a7bb01f39de5ba39b92335
Victor Goya <victor.goya@af83.com>
2013-08-08 23:18:18 +0200

    fix a few deploy stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,15 @@ set :application, "babot"
 set :user, "babot"
 set :appname, user
 
+require "rvm/capistrano"
+
+set :rvm_ruby_string, "2.0.0"
+
 require "capistrano/af83"
 
 set :repository,  "git@github.com:phorque/babot.git"
 set :scm, :git
+
+after 'deploy:update_code' do
+  run "cd #{current_path} && bundle exec rake bots:update"
+end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
-set :branch, :production
-set :application, "server.domain"
+set :branch, :master
+set :application, "babot.server"
 set :deploy_to,   "/home/babot"
 set :default_environment, { 'RAILS_ENV' => 'production' }
 server "#{user}@#{application}", :app, :primary => true
