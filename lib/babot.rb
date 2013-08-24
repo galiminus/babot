876530a68095e2db057932681d25f412fedf43ec
Victor Goya <victor.goya@af83.com>
2013-08-24 15:26:54 +0200

    put scheduling in babot

--- lib/babot.rb
+++ lib/babot.rb
@@ -4,4 +4,17 @@ class Babot
   def Babot.run!
     new.call
   end
+
+  def twitter
+    return @twitter if @twitter
+
+    options = YAML::load_file "config/credentials.yml"
+
+    @twitter = Twitter::REST::Client.new do |config|
+      config.consumer_key = options['consumer_key']
+      config.consumer_secret = options['consumer_secret']
+      config.oauth_token = options['oauth_token']
+      config.oauth_token_secret = options['oauth_token_secret']
+    end
+  end
 end
